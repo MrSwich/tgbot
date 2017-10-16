5a262e9a99b6c65bc8eb3ca9ea1089969e53a2b1
Артем Ядельський < >
2017-10-16 22:01:47 +0200

    tgbot

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
@@ -14,11 +10,23 @@ app.get('/', function(request, response) {
   response.render('pages/index')
 });
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+});
+
+
+
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '341606823:AAHOMPub5YoX86JKuGUYxA56abhYvdlQsyo';
+
+const bot = new TelegramBot(token, {polling: true});
+
+// Echo
+bot.onText(/\/echo (.+)/, (msg, match) => {
+	const chatId = msg.chat.id;
+
+	const resp = match[1];
+	bot.sendMessage(chatId, resp);
 });
 
